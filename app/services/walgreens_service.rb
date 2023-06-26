6a47b7c0816f15ff501e4941ff9102a15f1679f4
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-06-26 12:33:21 -0700

    first commit

---
+++ app/services/walgreens_service.rb
@@ -0,0 +1,9 @@
+class WalgreensService
+  include HTTParty
+
+  BASE_URL = 'https://services-qa.walgreens.com/api/stores/search/v2' 
+
+  def initialize; end
+
+  # Add other methods to connect to the API and get data
+end
